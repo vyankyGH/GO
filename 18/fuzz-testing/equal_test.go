fc9e5cd3b4520b653ac77966af20ff9908ff3a2b
Vyankatesh Kudtarkar <vyankateshkd@gmail.com>
2022-08-29 21:52:09 +0530

    add fuzz testing

---
+++ 18/fuzz-testing/equal_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "testing"
+
+func TestEqual(t *testing.T) {
+	if !Equal([]byte{'f', 'u', 'z', 'z'}, []byte{'f', 'u', 'z', 'z'}) {
+		t.Error("expected true, got false")
+	}
+}
+
+func FuzzEqual(f *testing.F) {
+	// target, can be only one per test
+	// values of a and b will be auto-generated
+	f.Fuzz(func(t *testing.T, a []byte, b []byte) {
+		Equal(a, b)
+	})
+}
