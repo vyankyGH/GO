424568b1766a379f1d99a056005fd6502e3dc14c
Vyankatesh Kudtarkar <vyankateshkd@gmail.com>
2022-08-30 08:52:15 +0530

    update

--- 18/generic.go
+++ 18/generic.go
@@ -15,20 +15,20 @@ func main() {
 	newGenericFunc(testAge2)
 }
 
-// type Age interface {
-// 	int64 | int32 | float32 | float64
-// }
+type Age interface {
+	int64 | int32 | float32 | float64
+}
 
-// func newGenericFunc[age Age](myAge age) {
-// 	val := int(myAge) + 1
-// 	fmt.Println(val)
-// }
+func newGenericFunc[age Age](myAge age) {
+	val := int(myAge) + 1
+	fmt.Println(val)
+}
 
-// func main() {
-// 	fmt.Println("Go Generics Tutorial")
-// 	var testAge int64 = 23
-// 	var testAge2 float64 = 24.5
+func main() {
+	fmt.Println("Go Generics Tutorial")
+	var testAge int64 = 23
+	var testAge2 float64 = 24.5
 
-// 	newGenericFunc(testAge)
-// 	newGenericFunc(testAge2)
-// }
+	newGenericFunc(testAge)
+	newGenericFunc(testAge2)
+}
